08cbf103b6dec6862e6a81037845e1b7396c381f
qustrolabe <qustrolabe@gmail.com>
2026-08-23 03:02:55 +0300

    add recommendations page with table styling

---
+++ src/content/recommendations/data.ts
@@ -0,0 +1,58 @@
+export interface Recommendation {
+    name: string;
+    type:
+        | "TV Series"
+        | "Video Game"
+        | "Manhwa"
+        | "Anime"
+        | "Movie"
+        | "Book"
+        | "Music"
+        | "Other";
+    year?: number;
+    description?: string;
+    rating?: number;
+}
+
+export const recommendations: Recommendation[] = [
+    {
+        name: "Pantheon",
+        type: "TV Series",
+        year: 2022,
+    },
+    {
+        name: "Outer Wilds",
+        type: "Video Game",
+        year: 2019,
+    },
+    {
+        name: "Baldur's Gate 3",
+        type: "Video Game",
+        year: 2023,
+    },
+    {
+        name: "Mookhyang Dark Lady",
+        type: "Manhwa",
+        year: 2019,
+    },
+    {
+        name: "Omniscient Reader's Viewpoint",
+        type: "Manhwa",
+    },
+    {
+        name: "SSS-Class Revival Hunter",
+        type: "Manhwa",
+    },
+    {
+        name: "Kino no Tabi: The Beautiful World",
+        type: "Anime",
+    },
+    {
+        name: "Serial Experiments Lain",
+        type: "Anime",
+    },
+    {
+        name: "There is no Antimemetics Division",
+        type: "Book",
+    },
+];
